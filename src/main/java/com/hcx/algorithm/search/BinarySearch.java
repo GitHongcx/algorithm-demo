f1378dbd52cbc8b76ce2855ab0d28c4aef9be51f
洪彩霞 <948957717@qq.com>
2024-05-02 15:34:34 +0800

    feature:Leetcode34.在排序数组中查找元素的第一个和最后一个位置 Leetcode35.搜索插入位置

--- src/main/java/com/hcx/algorithm/search/BinarySearch.java
+++ src/main/java/com/hcx/algorithm/search/BinarySearch.java
@@ -179,7 +179,7 @@ public class BinarySearch {
             } else {
                 //相等了不立刻返回，而是找到最右侧的，再返回
                 candidate = mid;
-                //继续向左查找，看看是否还有相同的
+                //继续向右查找，看看是否还有相同的
                 i = mid + 1;
             }
         }
@@ -231,6 +231,82 @@ public class BinarySearch {
         }
         return i - 1;
     }
+
+    /**
+     * Leetcode35. 搜索插入位置
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * 请必须使用时间复杂度为 O(log n) 的算法。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) >>> 1;
+            if (target <= nums[mid]) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i;
+    }
+
+
+    /**
+     * Leetcode34. 在排序数组中查找元素的第一个和最后一个位置
+     * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+     * 如果数组中不存在目标值 target，返回 [-1, -1]。
+     * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        // 查找最左侧元素
+        int left = searchMost(nums, target, -1);
+        if (left == -1) {
+            return new int[]{-1, -1};
+        } else {
+            // 查找最右侧元素
+            int right = searchMost(nums, target, 1);
+            return new int[]{left, right};
+        }
+    }
+
+    /**
+     * 最左/右
+     * @param nums
+     * @param target
+     * @param lr
+     * @return
+     */
+    public int searchMost(int[] nums,int target,int lr) {
+        int i = 0;
+        int j = nums.length - 1;
+        int candidate = -1;
+        while (i <= j) {
+            int mid = (i + j) >>> 1;
+            if (target < nums[mid]) {
+                j = mid - 1;
+            } else if (target > nums[mid]) {
+                i = mid + 1;
+            } else {
+                candidate = mid;
+                // 最左元素
+                if (lr == -1) {
+                    j = mid - 1;
+                } else {
+                    //最右元素
+                    i = mid + 1;
+                }
+            }
+        }
+        return candidate;
+    }
+
 
     /**
      * 从有序数组中找到num

f7fa2628acb76fab19c517a2d31f5bcd99af4c0e
洪彩霞 <948957717@qq.com>
2024-05-06 10:34:42 +0800

    chore:字符串匹配

---
+++ src/main/java/com/hcx/algorithm/arr/StringMatching.java
@@ -0,0 +1,15 @@
+package com.hcx.algorithm.arr;
+
+/**
+ * @Title: StringMatching.java
+ * @Package com.hcx.algorithm.arr
+ * @Description: Leetcode1408.数组中的字符串匹配
+ * 给你一个字符串数组 words ，数组中的每个字符串都可以看作是一个单词。请你按 任意 顺序返回 words 中是其他单词的子字符串的所有单词。
+ * 如果你可以删除 words[j] 最左侧和/或最右侧的若干字符得到 words[i] ，那么字符串 words[i] 就是 words[j] 的一个子字符串。
+ * @Author: hongcaixia
+ * @Date: 2024/5/6 10:33
+ * @Version V1.0
+ */
+public class StringMatching {
+
+}

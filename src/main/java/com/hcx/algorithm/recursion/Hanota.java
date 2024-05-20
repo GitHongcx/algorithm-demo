80a07ace4d22b14de5bf13cd21e4366c06693271
hongcaixia <948957717@qq.com>
2024-05-20 23:45:12 +0800

    feature:汉诺塔【递归】

---
+++ src/main/java/com/hcx/algorithm/recursion/Hanota.java
@@ -0,0 +1,119 @@
+package com.hcx.algorithm.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Title: Hanota.java
+ * @Package com.hcx.algorithm.recursion
+ * @Description: Leetcode面试题08.06.汉诺塔问题
+ * 在经典汉诺塔问题中，有 3 根柱子及 N 个不同大小的穿孔圆盘，盘子可以滑入任意一根柱子。一开始，所有盘子自上而下按升序依次套在第一根柱子上(即每一个盘子只能放在更大的盘子上面)。移动圆盘时受到以下限制:
+ * (1) 每次只能移动一个盘子;
+ * (2) 盘子只能从柱子顶端滑出移到下一根柱子;
+ * (3) 盘子只能叠在比它大的盘子上。
+ *
+ * 请编写程序，用栈将所有盘子从第一根柱子移到最后一根柱子。你需要原地修改栈。
+ *
+ * 示例1:
+ *  输入：A = [2, 1, 0], B = [], C = []
+ *  输出：C = [2, 1, 0]
+ *
+ *  示例2:
+ *  输入：A = [1, 0], B = [], C = []
+ *  输出：C = [1, 0]
+ *
+ *  A中盘子的数目不大于14个。
+ *
+ * @Author: hongcaixia
+ * @Date: 2024/5/20 20:55
+ * @Version V1.0
+ */
+public class Hanota {
+
+    /**
+     * A:[1,0]
+     * @param A
+     * @param B
+     * @param C
+     */
+    public static void hanota(List<Integer> A, List<Integer> B, List<Integer> C) {
+        remove(A.size(),A,B,C);
+    }
+
+    /**
+     * 1个圆盘以上的操作
+     * 规律：a->b  a->c  b->c
+     * @param count
+     * @param A
+     * @param B
+     * @param C
+     */
+    public static void remove1(int count, List<Integer> A, List<Integer> B, List<Integer> C) {
+
+        // 3个圆盘的操作
+        //前两个移动到B A->B  第n-1个圆盘的操作
+//        C.add(A.remove(A.size() - 1));
+//        B.add(A.remove(A.size() - 1));
+//        B.add(C.remove(C.size() - 1));
+
+        //第一步:A->B
+        remove1(count - 1, A, B, C);
+        remove1(count - 1, A, C, B);
+        remove1(count - 1, C, A, B);
+
+        //第3个移动到C A->C  第n个圆盘的操作
+        C.add(A.remove(A.size() - 1));
+
+        // 中间两个移动到C B->C 第n-1个圆盘的操作
+//        A.add(B.remove(B.size()-1));
+//        C.add(B.remove(B.size()-1));
+//        C.add(A.remove(A.size()-1));
+
+        remove1(count - 1, B, C, A);
+        remove1(count - 1, B, A, C);
+        remove1(count - 1, A, B, C);
+
+
+    }
+
+    public static void remove(int count, List<Integer> A, List<Integer> B, List<Integer> C) {
+        if (count == 0) {
+            return;
+        }
+        // A->B  前n-1个
+        // B.add(A.remove(A.size()));
+        System.out.println("前面第n-1个递归开始");
+        remove(count - 1, A, C, B);
+        System.out.println("前面第n-1个递归结束");
+        // A->C 第n个
+
+        System.out.println("第n个递归开始");
+        C.add(A.remove(A.size() - 1));
+        System.out.println("第n个递归结束");
+
+        // B->C 前n-1个
+        //C.add(B.remove(B.size()-1));
+        System.out.println("后面第n-1个递归开始");
+        remove(count - 1, B, A, C);
+        System.out.println("后面第n-1个递归结束");
+    }
+
+
+
+    public static void main(String[] args) {
+        List<Integer> A  = new ArrayList<>(3);
+
+        A.add(2);
+        A.add(1);
+        A.add(0);
+
+        List<Integer> B  = new ArrayList<>(3);
+        List<Integer> C  = new ArrayList<>(3);
+
+        hanota(A,B,C);
+
+        //hanota(A,B,C);
+
+        System.out.println(C);
+    }
+}

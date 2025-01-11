0bc5ddc05fa9f9063eeab7ac2d7454f616e7a341
洪彩霞 <948957717@qq.com>
2025-01-11 15:01:28 +0800

    feature:Leetcode622.设计循环队列（链表实现）

---
+++ src/main/java/com/hcx/algorithm/queue/CircularLinkedListQueue.java
@@ -0,0 +1,79 @@
+package com.hcx.algorithm.queue;
+
+/**
+ * @Title: CircularLinkedListQueue.java
+ * @Package com.hcx.algorithm.queue
+ * @Description: Leetcode622.设计循环队列
+ * @Author: hongcaixia
+ * @Date: 2025/1/11 14:57
+ * @Version V1.0
+ */
+public class CircularLinkedListQueue {
+
+    private static class Node {
+        int value;
+        Node next;
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+    private final Node head = new Node(-1, null);
+    private Node tail = head;
+    private int size = 0;
+    private int capacity = 0;
+
+    {
+        tail.next = head;
+    }
+
+    public CircularLinkedListQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean enQueue(int value) {
+        if(isFull()) {
+            return false;
+        }
+        Node added = new Node(value, head);
+        tail.next = added;
+        tail = added;
+        size++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if(isEmpty()) {
+            return false;
+        }
+        Node first = head.next;
+        head.next = first.next;
+        if (first == tail) {
+            tail = head;
+        }
+        size--;
+        return true;
+    }
+
+    public int Front() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return head.next.value;
+    }
+
+    public int Rear() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return tail.value;
+    }
+
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}

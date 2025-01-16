de08a8a049ba359f68b85634920465dc75a5b56c
洪彩霞 <948957717@qq.com>
2025-01-16 17:07:30 +0800

    feature:Leetcode101.对称二叉树

--- src/main/java/com/hcx/algorithm/tree/SymmetricTree.java
+++ src/main/java/com/hcx/algorithm/tree/SymmetricTree.java
@@ -3,8 +3,7 @@ package com.hcx.algorithm.tree;
 /**
  * @Title: SymmetricTree.java
  * @Package com.hcx.algorithm.tree
- * @Description: 对称二叉树
- *  https://leetcode.cn/problems/symmetric-tree/
+ * @Description: Leetcode101.对称二叉树
  *  给你一个二叉树的根节点 root ， 检查它是否轴对称。
  * @Author: hongcaixia
  * @Date: 2024/3/30 13:36
@@ -15,7 +14,6 @@ public class SymmetricTree {
     public static boolean isSymmetric(TreeNode root) {
         return isSame(root, root);
     }
-
 
     /**
      * 判断左右是否相等
@@ -31,6 +29,29 @@ public class SymmetricTree {
             return true;
         }
         return left.val == right.val && isSame(left.right, right.left) && isSame(left.left, right.right);
+    }
+
+
+    public boolean checkSymmetricTree(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return recursionCheck(root.left,root.right);
+    }
+
+    public boolean recursionCheck(TreeNode left,TreeNode right){
+        if(left==null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        if(left.val!=right.val){
+            return false;
+        }
+        boolean leftRes = recursionCheck(left.left,right.right);
+        boolean rightRes = recursionCheck(left.right,right.left);
+        return leftRes && rightRes;
     }
 
     public static void main(String[] args) {

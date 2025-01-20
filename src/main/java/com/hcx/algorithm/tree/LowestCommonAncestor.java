4af1f725e3518864f383700216766109439e0f75
洪彩霞 <948957717@qq.com>
2025-01-20 14:55:36 +0800

    feature:236.二叉树的最近公共祖先

--- src/main/java/com/hcx/algorithm/tree/LowestCommonAncestor.java
+++ src/main/java/com/hcx/algorithm/tree/LowestCommonAncestor.java
@@ -29,4 +29,39 @@ public class LowestCommonAncestor {
         }
         return node;
     }
+
+    /**
+     * 236.二叉树的最近公共祖先
+     * 在后序遍历（左右根）回溯的过程中进行处理
+     * 如果左子树出现了p、q 右子树出现了p、q，按照后序遍历的结果就找到了p和q，此时根节点就是最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        // 左子树出现了p、q
+        TreeNode leftTree = lowestCommonAncestor2(root.left, p, q);
+        // 右子树出现了p、q
+        TreeNode rightTree = lowestCommonAncestor2(root.right, p, q);
+        // 当前的root就是公共祖先
+        if (leftTree != null && rightTree != null) {
+            return root;
+        }
+        // 左子树有出现pq 右子树没出现 继续将公共祖先网上返回
+        if (leftTree != null) {
+            return leftTree;
+        }
+        // 右子树出现了pq，左子树没出现 继续将公共祖先网上返回
+        if (rightTree != null) {
+            return rightTree;
+        }
+        return null;
+    }
 }

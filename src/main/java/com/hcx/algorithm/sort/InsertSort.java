3cb7bd1ed9cfc57f0f5177ca06a1aeae3600316c
洪彩霞 <948957717@qq.com>
2025-01-28 16:06:12 +0800

    feature:插入排序

--- src/main/java/com/hcx/algorithm/sort/InsertSort.java
+++ src/main/java/com/hcx/algorithm/sort/InsertSort.java
@@ -16,10 +16,36 @@ package com.hcx.algorithm.sort;
 public class InsertSort {
 
     public static void main(String[] args) {
-        int[] arr = {5,8,1,0,3,2};
-        insertRecursion(arr,1);
+        //int[] arr = {5,8,1,0,3,2};
+        int[] arr = {9, 3, 7, 2, 5, 8, 1, 4, 6};
+        insertSort(arr);
         System.out.println(arr);
     }
+
+    /**
+     * 插入排序
+     * @param arr
+     */
+    public static void insertSort(int[] arr) {
+        // low为未排序区域的左边界，依次递增 ，取出来，插入到已排序区域
+        for (int low = 1; low < arr.length; low++) {
+            // 存入到临时变量中
+            int t = arr[low];
+            // 依次与左侧的已排序区域的元素比较，如果比左侧区域的小，就把左侧的区域往右移动，空出位置给t插入
+            int i = low - 1;
+            // i还在有效范围内， i所在元素值比t大，就把元素依次往右移动，空出位置
+            while (i >= 0 && arr[i] > t) {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            // 如果左侧的都比当前的t要小 说明t已经是在正确的位置了
+            if(i != low-1){
+                //i+1的位置就是空出的位置
+                arr[i + 1] = t;
+            }
+        }
+    }
+
 
     /**
      * 插入排序
